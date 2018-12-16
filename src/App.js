76f51f09121c01c17f07368ec21aca8e9a377056
wangxiaolong <wangxiaolong@zhuotaiju.com>
2018-12-16 21:17:09 +0800

    4-11实现css动画

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component, Fragment} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    }
+    this.handleToggole = this.handleToggole.bind(this);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+        <button onClick={this.handleToggole}>toggole</button>
+      </Fragment>
+    )
+  }
+
+  handleToggole() {
+    this.setState({
+      show: this.state.show ? false : true
+    })
+  }
+}
+
+export default App;
