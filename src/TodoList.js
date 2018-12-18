9de54d3bb47e0adc8b1bcde2602f0dd41a3fa968
wangxiaolong <wangxiaolong@zhuotaiju.com>
2018-12-18 18:35:04 +0800

    5-5 action 和 reducer编写

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,30 +1,54 @@
 import React, {Component} from 'react'
 import 'antd/dist/antd.css'
-import {Input, Button,List} from 'antd';
+import {Input, Button, List} from 'antd';
+import store from './store';
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handelStoreChange = this.handelStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handelStoreChange)
+  }
+
   render() {
     return (
-      <div style={{margin:'10px', marginLeft:'10'}}>
-        <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}/>
-        <Button type="primary">提交</Button>
+      <div style={{margin: '10px', marginLeft: '10'}}>
+        <Input
+          value={this.state.inputValue}
+          placeholder='todo info'
+          style={{width: '300px', marginRight: '10px'}}
+          onChange={this.handleInputChange}
+        />
+        <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
 
         <List
-          style={{marginTop:'10px', width:'300px'}}
+          style={{marginTop: '10px', width: '300px'}}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (<List.Item>{item}</List.Item>)}
         />
       </div>
     )
+  }
+  handleBtnClick(){
+    const action = {
+      type:'add_todo_item'
+    }
+    store.dispatch(action);
+  }
+  handelStoreChange(){
+    this.setState(store.getState());
+  }
+  handleInputChange(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
   }
 }
 
